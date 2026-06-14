a0855af0df30faa005c95963a04fd3a34a869d0c
ait meziane yanis <Yanis07.am@gmail.com>
2026-06-14 21:13:14 +0200

    Modification message Dev2

--- hello.py
+++ hello.py
@@ -2,9 +2,7 @@ from colorama import init, Fore, Style
 init()
 
 def display():
-
     message = f"{Fore.GREEN}Bonjour {Fore.CYAN}tout le monde !{Style.RESET_ALL}"
     print(message)
-
 if __name__ == "__main__":
     display()
